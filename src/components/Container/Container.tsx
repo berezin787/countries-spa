5e31232266441cb1bd6716f9cb6ad673d2e7cc38
alex <berezin787@gmail.com>
2024-07-17 13:00:35 +0300

    Setup base styles for Container UI component

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,12 +1,10 @@
 import { FC } from 'react';
-import classNames from 'classnames';
 import { Container as ChakraContainer } from '@chakra-ui/react';
-import styles from './Container.module.css';
 import { ContainerProps } from './Container.props.ts';
 
 export const Container: FC<ContainerProps> = ({ children }) => {
   return (
-    <ChakraContainer className={classNames(styles.container)}>
+    <ChakraContainer>
       {children}
     </ChakraContainer>
   );
---
+++ src/theme/components/Container/index.ts
@@ -0,0 +1,10 @@
+import { defineStyleConfig } from '@chakra-ui/react';
+
+export const containerTheme = defineStyleConfig({
+  baseStyle: {
+    'margin': '0 auto',
+    'padding': '0 2rem',
+    maxW: '100%',
+    w: '7xl',
+  },
+});
