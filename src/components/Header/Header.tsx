6bb534da5af33e9563593be5ce3c507b0c108762
alex <berezin787@gmail.com>
2024-07-17 20:52:43 +0300

    update Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,8 @@
 import { FC } from 'react';
 import classNames from 'classnames';
 import styles from './Header.module.css';
-import { Box, Flex, useColorMode } from '@chakra-ui/react';
+import { Box, Flex, IconButton, useColorMode } from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 import { Container } from '../Container/Container.tsx';
 
 export const Header: FC = () => {
@@ -12,7 +13,13 @@ export const Header: FC = () => {
         <Flex className={classNames(styles.header)}>
           <Box>Where is the world?</Box>
           <Box onClick={toggleColorMode}>
-            2
+            <IconButton
+              icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+              fontSize='sm'
+            />
+            <span
+              className={classNames(styles['color-mode-title'])}
+            >{colorMode} mode</span>
           </Box>
         </Flex>
       </Container>
