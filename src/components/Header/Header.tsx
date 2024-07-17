3a29d1f6f29eb3e5b709090d130dd94257d1ac1b
alex <berezin787@gmail.com>
2024-07-17 13:02:38 +0300

    Update MainLayout, set up Header Component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,21 @@
 import { FC } from 'react';
+import classNames from 'classnames';
+import styles from './Header.module.css';
+import { Box, Flex, useColorMode } from '@chakra-ui/react';
+import { Container } from '../Container/Container.tsx';
 
 export const Header: FC = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <>Header</>
+    <Box boxShadow='xl'>
+      <Container>
+        <Flex className={classNames(styles.header)}>
+          <Box>Where is the world?</Box>
+          <Box onClick={toggleColorMode}>
+            2
+          </Box>
+        </Flex>
+      </Container>
+    </Box>
   );
 };
--- src/layouts/Main/MainLayout.tsx
+++ src/layouts/Main/MainLayout.tsx
@@ -1,7 +1,5 @@
 import { FC } from 'react';
 import { Outlet } from 'react-router-dom';
-import classNames from 'classnames';
-import styles from './MainLayout.module.css';
 import { Header } from '../../components/Header/Header.tsx';
 import { Container } from '../../components/Container/Container.tsx';
 
@@ -9,11 +7,9 @@ export const MainLayout: FC = () => {
   return (
     <>
       <Header />
-      <div className={classNames(styles.wrapper)}>
-        <Container>
-          <Outlet />
-        </Container>
-      </div>
+      <Container>
+        <Outlet />
+      </Container>
     </>
   );
 };
