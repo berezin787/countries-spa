080c6d822566a29fbe1084986bcc653c3e04c3f9
alex <berezin787@gmail.com>
2024-07-17 12:58:22 +0300

    setup ColorModeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './react-query';
 import { theme } from './theme';
@@ -9,6 +9,7 @@ function App() {
   return (
     <ChakraProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <RouterProvider router={routes}></RouterProvider>
       </QueryClientProvider>
     </ChakraProvider>
