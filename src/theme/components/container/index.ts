39fbc6d604982ade32136a1805b8a6cd114251db
alex <berezin787@gmail.com>
2024-07-19 22:53:45 +0300

    add marginB to Container

--- src/theme/components/container/index.ts
+++ src/theme/components/container/index.ts
@@ -2,8 +2,8 @@ import { defineStyleConfig } from '@chakra-ui/react';
 
 export const Container = defineStyleConfig({
   baseStyle: {
-    'margin': '0 auto',
     'padding': '0 2rem',
+    'margin': { base: '0 auto 1rem', md: '0 auto 4rem' },
     maxW: '100%',
     w: '7xl',
   },
