8c545ce591a39ce677d93afb0e84c490626fbaeb
alex <berezin787@gmail.com>
2024-07-19 11:21:26 +0300

    Update Header component with Link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react';
 import classNames from 'classnames';
-import styles from './Header.module.css';
 import { Box, Flex, IconButton, useColorMode } from '@chakra-ui/react';
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import styles from './Header.module.css';
 import { Container } from '../Container/Container.tsx';
+import { Link } from '../Link/Link.tsx';
 
 export const Header: FC = () => {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -11,7 +12,9 @@ export const Header: FC = () => {
     <Box boxShadow='xl'>
       <Container>
         <Flex className={classNames(styles.header)}>
-          <Box>Where is the world?</Box>
+          <Box>
+            <Link to='/pew'>Where is the world?</Link>
+          </Box>
           <Box onClick={toggleColorMode}>
             <IconButton
               icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
