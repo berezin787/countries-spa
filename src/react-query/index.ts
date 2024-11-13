7270d678ad62f0e92396b67a6ceb6d3684cd6259
alex <berezin787@gmail.com>
2024-11-13 16:03:14 +0200

    configure query client

--- src/react-query/index.ts
+++ src/react-query/index.ts
@@ -1,3 +1,30 @@
-import { QueryClient } from '@tanstack/react-query';
+import { QueryCache, QueryClient } from '@tanstack/react-query';
+import { toast } from '../components';
 
-export const queryClient = new QueryClient();
+const errorHandler = (errorMessage: string) => {
+  const id = 'react-query-toast';
+  const message = `Could not fetch data: ${errorMessage || 'error connecting to server'}`;
+
+  if (!toast.isActive(id)) {
+    toast({
+      id,
+      isClosable: true,
+      title: message,
+      position: 'top-right',
+      status: 'error',
+      variant: 'subtle',
+    });
+  }
+};
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 900_000,
+      staleTime: 600_000,
+    },
+  },
+  queryCache: new QueryCache({
+    onError: (error) => errorHandler(error?.message),
+  }),
+});
