23b76850a4234311e36be84f2f8ee0aaa90788a5
alex <berezin787@gmail.com>
2024-11-13 16:02:31 +0200

    ..fix import

--- src/react-query/key-factories.ts
+++ src/react-query/key-factories.ts
@@ -1,4 +1,4 @@
-import { queryKeys } from './constants.ts';
+import { queryKeys } from './constants';
 
 export const generateCountryKey = (country: string) => [queryKeys.country, country];
 export const generateAlphaKey = (codes: string[]) => [queryKeys.alpha, codes.join(',')];
