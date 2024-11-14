7988127d7c974c91c6fc9e2f1a5d8175a79b8fac
alex <berezin787@gmail.com>
2024-11-14 12:53:53 +0200

    Add transitions to Home-page

--- src/components/Countries/Countries.tsx
+++ src/components/Countries/Countries.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { SimpleGrid, Text, useColorMode } from '@chakra-ui/react';
+import { ScaleFade, SimpleGrid, Text, useColorMode } from '@chakra-ui/react';
 import classNames from 'classnames';
 import { ICountry } from '../../interfaces/country.interface';
 import { ICountriesProps } from './Countries.props.interface';
@@ -16,22 +16,24 @@ export const Countries: FC<ICountriesProps> = ({
 
   return (
     <>
-      { !isLoading && !countries.length && <Text mt={10} align='center'>Countries not found :( </Text> }
-      <SimpleGrid
-        className={classNames(styles.countries)}
-        columns={{ base: 1, md: 2, lg: 4 }}
-        gap={{ base: '2rem', lg: '3rem', xl: '4rem' }}
-      >
-        { !!countries.length &&
-          countries.map((country: ICountry) => (
+      <ScaleFade in={!isLoading && !countries.length} unmountOnExit={true} initialScale={0.4}>
+        <Text align='center' fontSize='xl' mt={10}>Countries not found :(</Text>
+      </ScaleFade>
+      <ScaleFade in={!!countries.length} initialScale={0.8}>
+        <SimpleGrid
+          className={classNames(styles.countries)}
+          columns={{ base: 1, md: 2, lg: 4 }}
+          gap={{ base: '2rem', lg: '3rem', xl: '4rem' }}
+        >
+          { countries.map((country: ICountry) => (
             <Country
               key={country.name.common}
               country={country}
               colorMode={colorMode}
               onClick={() => navigate(`/country/${country.name.common}`)}
-            />
-          )) }
-      </SimpleGrid>
+            />))}
+        </SimpleGrid>
+      </ScaleFade>
     </>
   );
 };
