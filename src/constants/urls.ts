a9ae7b443fa0f8803a2a057a8a393c86ffb45a4d
alex <berezin787@gmail.com>
2024-11-15 09:12:02 +0200

    Change API-url

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,4 +1,4 @@
 export const API_URL = import.meta.env.VITE_API_BASE_URL;
-export const ALL_COUNTRIES_URL = `${API_URL}/independent?fields=name,capital,flags,population,region`;
+export const ALL_COUNTRIES_URL = `${API_URL}/all?fields=name,capital,flags,population,region`;
 export const searchByCountryUrl = (name: string) => `${API_URL}/name/${name}`;
 export const searchByCodes = (codes: string[]) => `${API_URL}/alpha?codes=${codes.join(',')}`;
